cc7a47c42a50b87eb0f0880d000c41638f84df7f
bonnysid <bonnysidworker@gmail.com>
2021-03-19 17:28:13 +0400

    init: start test spring mvc

---
+++ src/main/java/org/bonnysid/bloom/config/MySpringMvcDispatcherServletInit.java
@@ -0,0 +1,21 @@
+package org.bonnysid.bloom.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
